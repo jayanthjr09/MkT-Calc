47ed85a090886f1070ce41f6214b570cb0957ad3
jayanth_jr <jayanth.jr@dynacommerce.com>
2025-02-27 14:10:09 +0100

    test cases updated

--- tests/MktCalculatorTest.php
+++ tests/MktCalculatorTest.php
@@ -20,25 +20,30 @@ class MktCalculatorTest extends TestCase
         $readings = array_map(function ($temp) {
             $reading = new TemperatureReading();
             $reading->setTemperature($temp);
-            $reading->setTimestamp(new \DateTime());  // Use current timestamp, or set a fixed one
+            $reading->setTimestamp(new \DateTime()); 
             return $reading;
         }, $temperatures);
 
-        return new DataSet($readings);
+        $dataSet = new DataSet();
+        foreach ($readings as $reading) {
+            $dataSet->addTemperatureReading($reading);
+        }
+
+        return $dataSet;
     }
 
     public function testSingleTemperature()
     {
         $tempData = $this->createDataSet([25]);
         $result = $this->calculator->calculate($tempData);
-        $this->assertEquals(0, $result);
+        $this->assertEquals(25, $result);
     }
 
     public function testMultipleTemperatures()
     {
         $tempData = $this->createDataSet([25, 30, 35, 40]);
         $result = $this->calculator->calculate($tempData);
-        $expectedResult = 0;
+        $expectedResult = 34;
         $this->assertEquals($expectedResult, round($result));
     }
 
@@ -46,8 +51,8 @@ class MktCalculatorTest extends TestCase
     {
         $tempData = $this->createDataSet([-10, -5, 0, 5, 10]);
         $result = $this->calculator->calculate($tempData);
-        $expectedResult = 0;
-        $this->assertEqualsWithDelta($expectedResult, $result, 0.01);
+        $expectedResult = 3;
+        $this->assertEquals($expectedResult, round($result));
     }
 
     public function testZeroTemperatures()
